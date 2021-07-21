9e3f9a8fae4b01453741f1e5231e0cabf75b1ef5
Ash_360 <abbasiashin0@gmail.com>
2021-07-22 00:48:37 +0530

    Added Json Web Token, added into database as well

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -21,6 +22,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 })
 
 
@@ -33,6 +42,22 @@ userSchema.pre('save', async function(next) {
     }
     next();
 })
+
+// method is as sort of a session we used this property in auth while generating token
+userSchema.methods.generateAuthToken = async function() {
+    try {
+        const token = jwt.sign({_id: this._id}, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token: token});
+        const isSaved = await this.save();
+        if (isSaved) {
+            return token;
+        } else {
+            console.error("An error occurred while generating token");
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 require('../db/conn')
 
 const User = require('../model/userSchema')
@@ -13,8 +14,8 @@ const isUserLogged = (req, res, next) => {
 
 // Helper Methods
 function isRequestBodyEmpty(obj) {
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop))
             return false;
     }
     return JSON.stringify(obj) === JSON.stringify({});
@@ -27,7 +28,7 @@ router.get('/', (req, res) => {
     return res.send('Hello World');
 });
 
-router.get('/about', isUserLogged,(req, res) => {
+router.get('/about', isUserLogged, (req, res) => {
     res.send('Hello from about')
 })
 
@@ -36,46 +37,62 @@ router.get('/contact', (req, res) => {
     res.send('')
 })
 
-router.post('/signin', async(req, res) => {
-    console.log(req.body)
-    const {email, password} = req.body;
+router.post('/signin', async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
     if (email === '' || password === '') {
         res.status(422);
-        return res.json({error: "Please fill the fields properly"});
+        return res.json({
+            error: "Please fill the fields properly"
+        });
     }
-    if (!email || !password ) {
+    if (!email || !password) {
         res.status(422);
-        return res.json({error: "Please fill all the required fields"});
+        return res.json({
+            error: "Please fill all the required fields"
+        });
     }
     try {
-    const user = await User.findOne({email: email});
-    
-    if (user) {
-        const isPasswordMatching = await bcrypt.compare(password, user.password);
-        if (isPasswordMatching)
-            {res.status(200).json({success: 
-            "User login successful"
-        }) } else {
-            res.status(401).json({error: "Invalid username / password"})
 
+        const user = await User.findOne({
+            email: email
+        });
+
+        if (user) {
+            const isPasswordMatching = await bcrypt.compare(password, user.password);
+            const token = await user.generateAuthToken(); // returns a promise
+            // const 
+            if (isPasswordMatching) {
+                res.status(200).json({
+                    success: "User login successful"
+                })
+            } else {
+                res.status(401).json({
+                    error: "Invalid username / password"
+                })
+
+            }
+        } else {
+            res.status(401).json({
+                error: "Email does not exist"
+            })
         }
-    } else {
-        res.status(401).json({error: "Email does not exist"})
+    } catch (err) {
+        throw err;
     }
-} catch(err) {
-    throw err;
-}
-    res.send('')
+    // res.send('')
 })
 
-router.get('/signup', (req, res) => {
-    res.send('')
-})
+// router.get('/signup', (req, res) => {
+//     res.send('')
+// })
 
 // Method 1: Using Promises
 /*
 router.post('/register', (req, res) => {
-    console.log(req.body);
+    console.log('%cauth.js line:82 token', 'color: #007acc;', token);(req.body);
     const {name, email, phone, work, password, cpassword} = req.body;
 
     // TODO: Validate name, email and work using helper method
@@ -108,7 +125,7 @@ router.post('/register', (req, res) => {
             res.status(500).json({error:"Failed to register user"})
         })
     })
-    .catch((error) => {console.log(error)});
+    .catch((error) => {console.cconsole.log('%cauth.js line:115 token', 'color: #007acc;', token);(error)});
 
    res.json({message: req.body});
    
@@ -116,36 +133,62 @@ router.post('/register', (req, res) => {
 */
 
 // Method 2: Using async await
-router.post('/register', async(req, res) => {
-    console.log(req.body);
-    const {name, email, phone, work, password, cpassword} = req.body;
+router.post('/register', async (req, res) => {
+    (req.body);
+    const {
+        name,
+        email,
+        phone,
+        work,
+        password,
+        cpassword
+    } = req.body;
     if (isRequestBodyEmpty(req.body)) {
         return res.status(400).send('Empty Body is not expected');
     }
 
     if (!name || !email || !phone || !work || !password || !cpassword) {
         res.status(422);
-        return res.json({error: "Please fill all the required fields"});
+        return res.json({
+            error: "Please fill all the required fields"
+        });
     }
 
     try {
-        const userExist = await User.findOne({email: email});
+        const userExist = await User.findOne({
+            email: email
+        });
         if (userExist) {
-            return res.status(409).json({error: "Email already exists"});
+            return res.status(409).json({
+                error: "Email already exists"
+            });
         }
         if (password !== cpassword) {
-            return res.status(400).json({error:"Password and confirm password are not the same"});
+            return res.status(400).json({
+                error: "Password and confirm password are not the same"
+            });
         }
         // encrypt the password is done in userSchema in db using pre method
-        const user = new User({name, email, phone, work, password});
+        const user = new User({
+            name,
+            email,
+            phone,
+            work,
+            password
+        });
         const isRegisterSuccess = await user.save();
         if (isRegisterSuccess) {
-            res.status(201).json({message: "User registered successfully"})
+            res.status(201).json({
+                message: "User registered successfully"
+            })
         } else {
-            res.status(500).json({error:"Failed to register user"});
+            res.status(500).json({
+                error: "Failed to register user"
+            });
         }
-    } catch(err) {
-        console.log(err);
+    } catch (err) {
+        console.log('%cauth.js line:152 token', 'color: #007acc;', token);
+        (err);
     }
     // await const userExist = User.findOne({email: email});
 })
